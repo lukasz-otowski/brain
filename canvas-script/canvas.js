3009452f5a7db8b400406696e17e27878f36c281
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-19 22:52:41 +0200

    updating canvas for names and dividing lines

--- canvas-script/canvas.js
+++ canvas-script/canvas.js
@@ -18,3 +18,12 @@ ctx.bezierCurveTo(0, 50, 0, 400, 200, 375);
 ctx.bezierCurveTo(400, 400, 400, 50, 200, 25);
 ctx.lineWidth=5;
 ctx.stroke();
+
+ctx.moveTo(50, 220);
+//ctx.quadraticCurveTo(200, 300, 350, 220)
+ctx.bezierCurveTo(150, 200, 100, 250, 200, 260);
+ctx.bezierCurveTo(300, 250, 250, 200, 350, 220);
+ctx.stroke();
+ctx.moveTo(70, 310);
+ctx.bezierCurveTo(180, 350, 220, 350, 330, 310);
+ctx.stroke();
